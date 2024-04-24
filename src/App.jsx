4df34712ad76d312687f4c21ead6541a01fe589a
Amiir <amirlearn08@gmail.com>
2024-04-24 00:21:01 -0800

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import Nav from './components/Nav'
+import ShowCase from './components/ShowCase'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Nav/>
+    <ShowCase/>
+    </>
+  )
+}
+
+export default App
